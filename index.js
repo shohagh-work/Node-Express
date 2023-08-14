b848b40ee12629e8f08c204329025ae11dbd9adb
shohaghpikt <85873344+shohaghpikt@users.noreply.github.com>
2023-08-14 14:05:41 +0600

    update from office

--- index.js
+++ index.js
@@ -2,10 +2,13 @@ const express = require('express');
 
 const app = express();
 
+app.use(express.text());
+
 app.get('/', (req, res) => {
     res.send('This is home page');
 });
 app.post('/', (req, res) => {
+    console.log(req.body);
     res.send('This is home page using post method');
 });
 app.listen(3000, () => {
