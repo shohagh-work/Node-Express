989e306b466ef02b217c0097b227c27e704f8b2c
shohaghpikt <85873344+shohaghpikt@users.noreply.github.com>
2023-08-14 11:08:42 +0600

    update from office

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ const app = express();
 app.get('/', (req, res) => {
     res.send('This is home page');
 });
+app.post('/', (req, res) => {
+    res.send('This is home page using post method');
+});
 app.listen(3000, () => {
     console.log('listening on port 3000');
 });
