c8f6a37bd84bf573c5986345f9c032e27c07ba38
Shohagh <shohaghpikt@gmail.com>
2023-08-15 11:45:08 +0600

    from home

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.get('/', (req, res) => {
 });
 app.post('/', (req, res) => {
     console.log(req.body);
-    res.send('This is home page using post method');
+    res.send('This is home page using POST method');
 });
 app.listen(3000, () => {
     console.log('listening on port 3000');
