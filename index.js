cefa7ea1d25cb7ccf381d3ba0e03381d851032af
Shohagh <shohaghpikt@gmail.com>
2023-08-19 11:32:24 +0600

    updated from home

--- index.js
+++ index.js
@@ -1,25 +1,31 @@
 const express = require('express');
-const cookieParser = require('cookie-parser');
+const publicRouter = require('./publicRouter');
+const adminRouter = require('./adminRouter');
+// const cookieParser = require('cookie-parser');
 // const handler = require('./handler');
 
 const app = express(); // the main app
-const adminRouter = express.Router();
-app.use(cookieParser());
-app.use(express.json());
 
-const loggerWrapper = (option) =>
-    function (req, res, next) {
-        if (option.log) {
-            console.log(
-                `${new Date(Date.now()).toLocaleString()} - ${req.method} - ${req.originalUrl} - ${
-                    req.protocol
-                } - ${req.ip}`
-            );
-            next();
-        } else {
-            throw new Error('This is an error');
-        }
-    };
+app.use('/', publicRouter);
+app.use('/admin/', adminRouter);
+
+// app.use(cookieParser());
+// app.use(express.json());
+
+// const loggerWrapper = (option) =>
+//     function (req, res, next) {
+//         if (option.log) {
+//             console.log(
+// eslint-disable-next-line max-len
+//                 `${new Date(Date.now()).toLocaleString()} - ${req.method} - ${req.originalUrl} - ${
+//                     req.protocol
+//                 } - ${req.ip}`
+//             );
+//             next();
+//         } else {
+//             throw new Error('This is an error');
+//         }
+//     };
 
 /* const looger = (req, res, next) => {
     console.log(
@@ -31,24 +37,24 @@ const loggerWrapper = (option) =>
     throw new Error('This is an error');
 }; */
 
-adminRouter.use(loggerWrapper({ log: false }));
+// adminRouter.use(loggerWrapper({ log: false }));
 
-adminRouter.get('/dashboard/', (req, res) => {
-    res.send('This is admin Dashboard');
-});
+// adminRouter.get('/dashboard/', (req, res) => {
+//     res.send('This is admin Dashboard');
+// });
 
-app.use('/admin/', adminRouter);
+// app.use('/admin/', adminRouter);
 
-app.get('/about/', (req, res) => {
-    res.send('This is about');
-});
+// app.get('/about/', (req, res) => {
+//     res.send('This is about');
+// });
 
-const errorMiddleware = (err, req, res, next) => {
-    console.log(err.message);
-    res.status(500).send('There was a server side error!');
-};
+// const errorMiddleware = (err, req, res, next) => {
+//     console.log(err.message);
+//     res.status(500).send('There was a server side error!');
+// };
 
-app.use(errorMiddleware);
+// app.use(errorMiddleware);
 // const admin = express();
 // app.use(express.json());
 // app.use(cookieParser());
---
+++ publicRouter.js
@@ -0,0 +1,38 @@
+const express = require('express');
+
+const publicRouter = express.Router();
+
+/* publicRouter.all('*', (req, res, next) => {
+    console.log('I am logging');
+    next();
+}); */
+
+// publicRouter.param('user', (req, res, next, id) => {
+//     req.user = id === '1' ? 'admin' : 'annoymous';
+//     console.log('I am logging');
+//     next();
+// });
+
+publicRouter.param((param, option) => (req, res, next, val) => {
+    if (option === val) {
+        console.log();
+        next();
+    } else {
+        res.send(403);
+    }
+});
+
+publicRouter.param('user', '12');
+
+publicRouter.get('/:user', (req, res, next) => {
+    res.send(`Hi ${req.user}`);
+    console.log('I am user');
+    next();
+});
+
+publicRouter.get('/:user/about', (req, res) => {
+    res.send(`Hi ${req.user}
+     This is About page`);
+});
+
+module.exports = publicRouter;
