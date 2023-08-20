5fefe817493412e8da46f024c9b7a57743b68103
shohaghpikt <85873344+shohaghpikt@users.noreply.github.com>
2023-08-20 13:13:28 +0600

    update from office

--- publicRouter.js
+++ publicRouter.js
@@ -2,6 +2,25 @@ const express = require('express');
 
 const publicRouter = express.Router();
 
+publicRouter
+    .route('/user')
+    .all((req, res, next) => {
+        console.log('I am logging something');
+        next();
+    })
+    .get((req, res) => {
+        res.send('GET');
+    })
+    .post((req, res) => {
+        res.send('post');
+    })
+    .put((req, res) => {
+        res.send('put');
+    })
+    .delete((req, res) => {
+        res.send('delete');
+    });
+
 /* publicRouter.all('*', (req, res, next) => {
     console.log('I am logging');
     next();
@@ -13,26 +32,26 @@ const publicRouter = express.Router();
 //     next();
 // });
 
-publicRouter.param((param, option) => (req, res, next, val) => {
-    if (option === val) {
-        console.log();
-        next();
-    } else {
-        res.send(403);
-    }
-});
+// publicRouter.param((param, option) => (req, res, next, val) => {
+//     if (option === val) {
+//         console.log();
+//         next();
+//     } else {
+//         res.send(403);
+//     }
+// });
 
-publicRouter.param('user', '12');
+// publicRouter.param('user', '12');
 
-publicRouter.get('/:user', (req, res, next) => {
-    res.send(`Hi ${req.user}`);
-    console.log('I am user');
-    next();
-});
+// publicRouter.get('/:user', (req, res, next) => {
+//     res.send(`Hi ${req.user}`);
+//     console.log('I am user');
+//     // next();
+// });
 
-publicRouter.get('/:user/about', (req, res) => {
-    res.send(`Hi ${req.user}
-     This is About page`);
-});
+// publicRouter.get('/:user/about', (req, res) => {
+//     res.send(`Hi ${req.user}
+//      This is About page`);
+// });
 
 module.exports = publicRouter;
